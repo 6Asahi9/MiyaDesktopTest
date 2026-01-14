a660fcf66753d46915387752e725de4bb047ee4b
Asahi <161689859+6Asahi9@users.noreply.github.com>
2026-01-14 18:59:23 +0530

    making exe

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QIcon
 import sys
 from ui.main_window import MainWindow
+import os
+
+def resource_path(relative_path):
+    if hasattr(sys, "_MEIPASS"):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+
+    app.setWindowIcon(QIcon(resource_path("assets/miya.ico")))
+
     window = MainWindow()
     window.show()
+
     sys.exit(app.exec())
